6fe5bbee50fd7fa7cf3017cca43f565fc77e8cbb
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2023-08-27 17:41:22 +0900

    Initial Commit

---
+++ Tax Calculator/Tax_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tax_CalculatorApp.swift
+//  Tax Calculator
+//
+//  Created by Paul Zamshin on 2023/08/27.
+//
+
+import SwiftUI
+
+@main
+struct Tax_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
